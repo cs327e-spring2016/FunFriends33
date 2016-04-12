a2aad22bd0c24021a4a6c363f2392a160b854fe5
Calahown <blind.noldor@gmail.com>
2016-04-11 19:46:34 -0500

    this file creates the database as rngmotors
    If you use this dont forget to change the password at the top

--- databasecreator.py
+++ databasecreator.py
@@ -1,7 +1,7 @@
 import pymysql
 conn = pymysql.connect(host='localhost', port = 3306,
                         user = 'root', passwd='lenneth6')
-
+    
 cur = conn.cursor()
 cur.execute("create database rngmotors")
 cur.execute("Use rngmotors")
@@ -19,43 +19,35 @@ cur.execute("CREATE TABLE colors" +
 cur.execute("CREATE TABLE inventory" +
             "(vin_number VARCHAR(16)," +
             "model_name VARCHAR(20)," +
-            "year SMALLINT UNSIGNED," +
-            "mileage INT UNSIGNED," +
-            "transmission ENUM('A','F')," +
-            "num_doors SMALLINT UNSIGNED," +
-            "condition VARCHAR(140)," +
-            "city_fuel_economy SMALLINT UNSIGNED," +
-            "hwy_fuel_economy SMALLINT UNSIGNED," +
-            "price INT UNSIGNED," +
-            "price_notes VARCHAR(140)," +
-            "ext_color_id SMALLINT UNSIGNED," +
-            "int_color_id SMALLINT UNSIGNED," +
-            "CONSTRAINT vn PRIMARY KEY (vin_number)" +
-            "CONSTRAINT model FOREIGN KEY (model_name)" +
-            "REFERENCES models (modelname)" +
-            "CONSTRAINT color FOREIGN KEY (ext_color_id)" +
-            "REFERENCES colors (color_id)" +
-            "CONSTRAINT color FOREIGN KEY (int_color_id" +
-            "REFERENCES colors (color_id)" +
+            "CONSTRAINT vn PRIMARY KEY (vin_number), " +
+            "CONSTRAINT mdl FOREIGN KEY (model_name) " +
+            "REFERENCES models (model_name)" +
             ");")
+
+cur.execute("ALTER TABLE inventory " +
+            "ADD year SMALLINT UNSIGNED," +
+            "ADD mileage INT UNSIGNED," +
+            "ADD transmission ENUM('A','F')," +
+            "ADD num_doors SMALLINT UNSIGNED," +
+            "ADD conditions VARCHAR(140)" )
+
+cur.execute("Alter Table inventory " +
+            "ADD city_fuel_economy SMALLINT UNSIGNED," +
+            "ADD hwy_fuel_economy SMALLINT UNSIGNED," +
+            "ADD price INT UNSIGNED," +
+            "ADD price_notes VARCHAR(140)")
+
+cur.execute("ALTER TABLE inventory " +
+            "ADD ext_color_id SMALLINT UNSIGNED")
+
+cur.execute("ALTER TABLE inventory " +
+            "ADD CONSTRAINT colorext FOREIGN KEY (ext_color_id) " +
+            "REFERENCES colors (color_id)" )
+
+cur.execute("ALTER TABLE inventory " +
+            "ADD int_color_id SMALLINT UNSIGNED," +
+            "ADD CONSTRAINT colorint FOREIGN KEY (int_color_id) " +
+            "REFERENCES colors (color_id)")
             
 cur.close()
 conn.close()
-
-
-
-
-
-
-
-#conn  = pymysql.connect(host='localhost', port = 3306,
-#                        user = 'root', passwd='lenneth6', db = 'bank')
-
-
-#cur = conn.cursor()
-
-#cur.execute("Use bank")
-#cur.execute("Select * FROM person WHERE person_id=1")
-#print(cur.fetchone())
-#cur.close()
-#conn.close()
