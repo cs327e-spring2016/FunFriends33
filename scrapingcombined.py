4968d6cc7f3e283db0bf72eeb3129b46e0520921
Calahown <blind.noldor@gmail.com>
2016-04-18 19:44:51 -0500

    new version
    Needs nulls and price tags

--- scrapingcombined.py
+++ scrapingcombined.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pymysql
 
 def scrape ():
 
@@ -19,23 +20,75 @@ def scrape ():
     for i in range(len(url_extensions)) :
         current = url_extensions[i]
         url_extension = current.attrs['href']
-        print(url_extension)
         extended_url = 'http://www.rangermotorsaustin.com' + url_extension
         urls_to_scrape.append(extended_url)
 
     # now print out the attributes of each car in turn
     for i in range(len(urls_to_scrape)):
         car_url = urls_to_scrape[i]
-        print (car_url)
         html = urlopen(str(car_url))
         bsObj = BeautifulSoup(html.read(), 'html.parser')
-        #attribute = bsObj.findAll('span', {'class':'strong'})
+        attribute = bsObj.findAll('span', {'class':'strong'})
         value = bsObj.findAll('span', {'class':'pull-right'})
         aaa_list = bsObj.findAll('li', {'class':'list-group-item'})
 
+
+
         value2 = []
+        attribute2 = []
         for y in value :
             value2.append(y.get_text())
-            print (value2)
+        for z in attribute :
+            attribute2.append(z.get_text())
 
-scrape()
+
+        dbwrite(value2, attribute2)
+
+#values and attributes are pairwise the same length
+#but they are not the same length all the time, so we need to check the attributes that a given car has before entering.
+#If a value is not there we need to add null instead
+#fill the values with null.
+
+def dbwrite (values, attributes):
+
+    conn = pymysql.connect(host='localhost', port = 3306,
+                        user = 'root', passwd='lenneth6')
+
+    cur = conn.cursor()
+
+    cur.execute("use rngmotors")
+
+    cur.execute("SELECT vin_number from inventory")
+
+
+    #this checks that the vin number is not already in the table
+    vinlist = list(cur.fetchall())
+    for i in range(len(vinlist)) :
+        if vinlist[i] == attributes[12] :
+            return
+
+    print(len(attributes))
+    print (attributes)
+    #attribute[12] is the vin number
+    stringer = str("INSERT INTO inventory (vin_number, model_name, ext_color_id, int_color_id) VALUES (" + attributes[12] +"," + attributes[2] + "," +  attributes[9] + "," + attributes[10] + ")")
+    stringer2 = str("UPDATE inventory set year =" + attributes[0] + ", mileage =" + attributes[4] + ", conditions = " + attributes[14] + "WHERE vin_number = " + attributes[12])
+
+    print (stringer)
+    print (stringer2)
+
+    #id 13 to not be in the database
+
+    cur.close()
+
+    conn.close()
+
+
+def main():
+    #ask the user what they want to do
+    #if data queary
+    #if update table scrape
+    #if update validation
+    scrape()
+
+
+main()
