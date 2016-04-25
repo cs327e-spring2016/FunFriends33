9207a1595754099dbe68367de051d2c47d20248d
ednedted <ned.vanniman@gmail.com>
2016-04-24 19:26:37 -0500

    Last time i committed the wrong files
    these are the right ones

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,8 @@
-##### SOME CLEAN SHIT ;) 
-##### NOTE:	
-##### THE WORD LIST FEELS OVERLOADED, 
-##### LIKE ITS SOME Semantic satiation SHIT AFTER A WHILe
-##### MAYBE WE CAN CHANGE SOME VARIABLE NAMES
+##### SOME CLEAN STUFF ;) 
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pymysql
-
 
 # scrape all listing urls
 
@@ -25,7 +20,7 @@ list_of_listing_dictionaries = []
 
 # now for each individual listing : scrape all available key/value pairs into a dictionary and append the dictionary to our list of listing dictionaries
 # IMPORTANT --------- STILL NEED TO SCRAPE THE PRICE
-for url in listing_urls[:3] :
+for url in listing_urls :
 	u = urlopen(url)
 	soup_listing = BeautifulSoup(u.read(), 'html.parser')
 
@@ -40,9 +35,15 @@ for url in listing_urls[:3] :
 		val = span_tags[1].get_text().strip()
 		listing_dictionary[key] = val
 
+	price = soup_listing.find('span', {'class':'details-price'}).get_text()
+	price = price.split()[2]
+	price = price.strip().replace(',', '').replace('$', '')
+	print(price)
+	listing_dictionary['Price'] = price
+
 	list_of_listing_dictionaries.append(listing_dictionary)
 
-list_of_keys = ['Year', 'Make', 'Model', 'Body style', 'Mileage', 'Transmission', 'Engine', 'Drivetrain', 'Exterior', 'Interior', 'Doors', 'Stock', 'VIN', 'Fuel Mileage', 'Conditon']
+list_of_keys = ['Price', 'Year', 'Make', 'Model', 'Body style', 'Mileage', 'Transmission', 'Engine', 'Drivetrain', 'Exterior', 'Interior', 'Doors', 'Stock', 'VIN', 'Fuel Mileage', 'Conditon']
 
 # fill in nulls
 for l in list_of_listing_dictionaries :
@@ -50,21 +51,28 @@ for l in list_of_listing_dictionaries :
 		if not (k in l) :
 			l[k] = 'NULL'
 
+# fix data types
+for l in list_of_listing_dictionaries :
+	if not (l['Doors'] == 'NULL') :
+		l['Doors'] = l['Doors'][0]
+	l['Mileage'] = l['Mileage'].replace(',', '')
+
+
 # create insert statement for each car
 
 list_of_insert_statements = []
 for l in list_of_listing_dictionaries :
 #		THIS IS WHERE YOU CHANGE THE TABLE INTO WHICH YOU INSERT RECORDS
-	insert_statement = 'INSERT INTO bbbb SET '
+	insert_statement = 'INSERT INTO dddd SET '
 	for key in l :
 		if key == 'Body style' :
 			insert_statement = insert_statement + 'Body_Style=("' + l[key] + '"), '
 		elif key == 'Fuel Mileage' :
 			insert_statement = insert_statement + 'Fuel_Mileage=("' + l[key] + '"), '
 		else :
-			insert_statement = insert_statement + key + '=("' + l[key] + '"), '	
+			if not (l[key] == 'NULL') :
+				insert_statement = insert_statement + key + '=("' + l[key] + '"), '	
 	insert_statement = insert_statement[:-2]
-	print('GOOD WORK!')
 	list_of_insert_statements.append(insert_statement)
 
 # maybe we should check the table for what vin numbers are already entries and update those?
@@ -74,7 +82,25 @@ conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='ro
 
 cur = conn.cursor()
 #		THIS IS WHERE YOU CHANGE THE DATABASE USED
-cur.execute('USE cars')
+cur.execute('CREATE DATABASE FunFriends33')
+cur.execute('USE FunFriends33')
+cur.execute('''CREATE TABLE carsForSale (
+	VIN varchar(30) primary key,
+	Price int,
+	Year year,
+	Make varchar(30),
+	Model varchar(30),
+	Body_Style varchar(30),
+	Mileage int,
+	Transmission varchar(30),
+	Engine varchar(30),
+	Drivetrain varchar(30),
+	Exterior varchar(30),
+	Interior varchar(30),
+	Doors int,
+	Stock varchar(30),
+	Fuel_Mileage varchar(30),
+	Conditon varchar(30))''')
 for ins_st in list_of_insert_statements :
 	cur.execute(ins_st)
 cur.close()
